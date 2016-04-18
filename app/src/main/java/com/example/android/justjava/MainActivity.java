772a4e012b331f576ba66daa968947f0a5d7a92d
Fred Deichler <(fred.deichler@gmail.com)>
2016-04-17 21:47:22 -0500

    added Chocolate

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,15 +1,9 @@
 package com.example.android.justjava;
 
-import android.net.Uri;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.TextView;
-
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
 
 /**
  * This app displays an order form to order coffee.
@@ -19,20 +13,7 @@ public class MainActivity extends AppCompatActivity {
     int quantity = 0;
     int priceOfOneCup = 5;
     boolean addWhippedCream = false;
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
-    }
+    boolean addChocolate = false;
 
     /**
      * This method is called when the order button is clicked.
@@ -40,8 +21,10 @@ public class MainActivity extends AppCompatActivity {
     public void submitOrder(View view) {
 
         CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
-        boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
-        displayMessage(createOrderSummary(quantity, hasWhippedCream));
+        CheckBox chocolateChckBox = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        addWhippedCream = whippedCreamCheckBox.isChecked();
+        addChocolate = chocolateChckBox.isChecked();
+        displayMessage(createOrderSummary(quantity));
     }
 
     /**
@@ -88,52 +71,14 @@ public class MainActivity extends AppCompatActivity {
         orderSummaryTextView.setTextSize(24);
     }
 
-    private String createOrderSummary(int number, boolean addWhippedCream) {
+    private String createOrderSummary(int number) {
         String orderSummary = "Name: Fred Deichler";
         orderSummary += "\nAdd whipped cream? " + addWhippedCream;
+        orderSummary += "\nAdd chocolate? " + addChocolate;
         orderSummary += "\nQuantity: " + number;
         orderSummary += "\nTotal: $" + calculatePrice();
         orderSummary += "\nThank you!";
         return orderSummary;
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://com.example.android.justjava/http/host/path")
-        );
-        AppIndex.AppIndexApi.start(client, viewAction);
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app URL is correct.
-                Uri.parse("android-app://com.example.android.justjava/http/host/path")
-        );
-        AppIndex.AppIndexApi.end(client, viewAction);
-        client.disconnect();
-    }
 }
